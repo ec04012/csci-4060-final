5af5b1463cbe450f0a991203d4585e0e5ed6891c
ec04012 <ec04012@uga.edu>
2022-11-30 16:45:30 -0500

    Created Ride object

---
+++ app/src/main/java/edu/uga/cs/ridesharefirebase/Ride.java
@@ -0,0 +1,92 @@
+package edu.uga.cs.ridesharefirebase;
+
+/**
+ * This class represents a single ride, either an offer or a request.
+ */
+public class Ride {
+
+        private String driver;;
+        private String sourceCity;
+        private String sourceState;
+        private String sourceZip;
+        private String destinationCity;
+        private String destinationState;
+        private String destinationZip;
+        private String car;
+
+        public Ride()
+        {
+            this.driver = null;
+            this.sourceCity = null;
+            this.sourceState = null;
+            this.sourceZip = null;
+            this.destinationCity = null;
+            this.destinationState = null;
+            this.destinationZip = null;
+            this.car = null;
+        }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getSourceCity() {
+        return sourceCity;
+    }
+
+    public void setSourceCity(String sourceCity) {
+        this.sourceCity = sourceCity;
+    }
+
+    public String getSourceState() {
+        return sourceState;
+    }
+
+    public void setSourceState(String sourceState) {
+        this.sourceState = sourceState;
+    }
+
+    public String getSourceZip() {
+        return sourceZip;
+    }
+
+    public void setSourceZip(String sourceZip) {
+        this.sourceZip = sourceZip;
+    }
+
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    public void setDestinationCity(String destinationCity) {
+        this.destinationCity = destinationCity;
+    }
+
+    public String getDestinationState() {
+        return destinationState;
+    }
+
+    public void setDestinationState(String destinationState) {
+        this.destinationState = destinationState;
+    }
+
+    public String getDestinationZip() {
+        return destinationZip;
+    }
+
+    public void setDestinationZip(String destinationZip) {
+        this.destinationZip = destinationZip;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+} // Ride class
