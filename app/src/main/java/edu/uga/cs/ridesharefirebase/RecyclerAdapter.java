ebce613974bdb480a771a059b308e0f2ce2b1565
cmk03560 <74875288+cmk03560@users.noreply.github.com>
2022-12-04 22:12:35 -0500

    asks user for name on register and working on making so user doesnt see their own request/offer on the browse

--- app/src/main/java/edu/uga/cs/ridesharefirebase/RecyclerAdapter.java
+++ app/src/main/java/edu/uga/cs/ridesharefirebase/RecyclerAdapter.java
@@ -1,6 +1,7 @@
 package edu.uga.cs.ridesharefirebase;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -41,12 +47,16 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         holder.rideSourceState.setText(ride.getSourceState());
         holder.rideDestCity.setText(ride.getDestinationCity());
         holder.rideDestState.setText(ride.getDestinationState());
-        holder.rider.setText("Rider:" + ride.getRider());
+
+       holder.rider.setText("Rider:" + ride.getRider());
         holder.driver.setText("Driver: " +ride.getDriver());
         //holder.rideDate.setText(ride.getDate());
 
         //bascially a pseudo check to see if the ride is an request since they dont need a car
         // would instead on final check to see if driver is null/false/no UID
+
+
+
         if(ride.getCar() == "") {
             holder.carTitle.setVisibility(View.INVISIBLE);
             holder.rideCar.setVisibility(View.INVISIBLE);
@@ -96,21 +106,20 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
                         //meaning the list of ride's rid matches what we clicked
                         if(fbRideList.get(i).getKey() == rideIdview.getText()) {
                             reserveRide = fbRideList.get(i);
-
                             Toast.makeText(view.getContext(), "resrveRide rid = " + reserveRide.getKey(), Toast.LENGTH_SHORT).show();
 
                             //meaning its a request
-                            if (reserveRide.getDriver() == "") {
+                            if (reserveRide.getDriver().equals("")) {
                                 Toast.makeText(view.getContext(), "set a new driver ", Toast.LENGTH_SHORT).show();
-                                reserveRide = fbRideList.get(i);
                                 reserveRide.setDriver(mFirebaseAuth.getCurrentUser().getUid());
-
+                                FirebaseUtil.updateRide(reserveRide);
                                 Toast.makeText(view.getContext(), reserveRide.toString(), Toast.LENGTH_LONG).show();
                             } // if ride is a request
                             //meaning this is a offer
                             else {
                                 Toast.makeText(view.getContext(), "set a new rider ", Toast.LENGTH_SHORT).show();
                                 reserveRide.setRider(mFirebaseAuth.getCurrentUser().getUid());
+                                FirebaseUtil.updateRide(reserveRide);
                                 Toast.makeText(view.getContext(), reserveRide.toString(), Toast.LENGTH_LONG).show();
                             } // if ride is an offer
                         } // if ride is the ride that we've have clicked
--- app/src/main/java/edu/uga/cs/ridesharefirebase/RegisterActivity.java
+++ app/src/main/java/edu/uga/cs/ridesharefirebase/RegisterActivity.java
@@ -27,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private EditText emailEditText;
     private EditText passwordEditText;
+    private EditText registerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         emailEditText = findViewById( R.id.register_email_editText);
         passwordEditText = findViewById( R.id.register_password_editText);
+        registerName = findViewById(R.id.registerName);
 
         Button registerButton = findViewById(R.id.register_account_button);
         registerButton.setOnClickListener( new RegisterButtonClickListener() );
@@ -43,50 +45,57 @@ public class RegisterActivity extends AppCompatActivity {
     private class RegisterButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+            if (emailEditText.getText().toString().equals("") && passwordEditText.getText().toString().equals("") && registerName.getText().toString().equals("")) {
+                Toast.makeText( getApplicationContext(), "please fill out the rest of the form to make an account.",  Toast.LENGTH_SHORT ).show();
+            } else {
+                final String email = emailEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
+                final String name = registerName.getText().toString();
+
+                final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+                // This is how we can create a new user using an email/password combination.
+                // Note that we also add an onComplete listener, which will be invoked once
+                // a new user has been created by Firebase.  This is how we will know the
+                // new user creation succeeded or failed.
+                // If a new user has been created, Firebase already signs in the new user;
+                // no separate sign in is needed.
+                firebaseAuth.createUserWithEmailAndPassword( email, password )
+                        .addOnCompleteListener( RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText( getApplicationContext(),
+                                            "Registered user: " + email,
+                                            Toast.LENGTH_SHORT ).show();
+
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d( DEBUG_TAG, "createUserWithEmail: success" );
+
+                                    // get current user, and create equivalent User pojo
+                                    FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                                    User userPojo = new User(firebaseUser);
+                                    userPojo.setName(name);
+                                    userPojo.setPoints(300);
+
+                                    // write user and points to firebase
+                                    FirebaseUtil.addUserToFirebase(userPojo);
+
+                                    Intent intent = new Intent( RegisterActivity.this, HomeActivity.class );
+                                    startActivity( intent );
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(DEBUG_TAG, "createUserWithEmail: failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Registration failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            } // onCompleteListener()
+                        }); // firebaseAuth.createUserWithEmailAndPassword( email, password ).addOnCompleteListener
+            }
             // get Strings from edit texts
-            final String email = emailEditText.getText().toString();
-            final String password = passwordEditText.getText().toString();
 
-            final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-
-            // This is how we can create a new user using an email/password combination.
-            // Note that we also add an onComplete listener, which will be invoked once
-            // a new user has been created by Firebase.  This is how we will know the
-            // new user creation succeeded or failed.
-            // If a new user has been created, Firebase already signs in the new user;
-            // no separate sign in is needed.
-            firebaseAuth.createUserWithEmailAndPassword( email, password )
-                    .addOnCompleteListener( RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-
-                                Toast.makeText( getApplicationContext(),
-                                        "Registered user: " + email,
-                                        Toast.LENGTH_SHORT ).show();
-
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d( DEBUG_TAG, "createUserWithEmail: success" );
-
-                                // get current user, and create equivalent User pojo
-                                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                                User userPojo = new User(firebaseUser);
-                                userPojo.setPoints(300);
-
-                                // write user and points to firebase
-                                FirebaseUtil.addUserToFirebase(userPojo);
-
-                                Intent intent = new Intent( RegisterActivity.this, HomeActivity.class );
-                                startActivity( intent );
-
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w(DEBUG_TAG, "createUserWithEmail: failure", task.getException());
-                                Toast.makeText(RegisterActivity.this, "Registration failed.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        } // onCompleteListener()
-                    }); // firebaseAuth.createUserWithEmailAndPassword( email, password ).addOnCompleteListener
         } // RegisterButtonClickListener.onClick()
     } // RegisterButtonClickListener
 
